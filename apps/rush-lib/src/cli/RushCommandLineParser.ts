00fadea180643f920d1d44437d51f9ea4a14b315
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2022-04-07 18:27:07 -0700

    Clean up the Telemetry flushing.

--- apps/rush-lib/src/cli/RushCommandLineParser.ts
+++ apps/rush-lib/src/cli/RushCommandLineParser.ts
@@ -168,9 +168,7 @@ export class RushCommandLineParser extends CommandLineParser {
   }
 
   public flushTelemetry(): void {
-    if (this.telemetry) {
-      this.telemetry.flush();
-    }
+    this.telemetry?.flush();
   }
 
   public async execute(args?: string[]): Promise<boolean> {
